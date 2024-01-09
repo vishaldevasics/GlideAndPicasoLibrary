60b21b41a9ba99dd29173aa8434aefacd45bb0b3
vishaldevasics <vishaldevasics@gmail.com>
2024-01-09 23:44:21 +0530

    Initial commit

---
+++ app/src/main/java/com/maverickallies/glideandpicasolibrary/MainActivity.java
@@ -0,0 +1,32 @@
+package com.maverickallies.glideandpicasolibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLoadImg;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnLoadImg = (Button)findViewById(R.id.btnLoadNewImgId);
+        imageView = (ImageView)findViewById(R.id.imgViewGlideId);
+
+
+
+    }
+    public void LoadNewImage(View view){
+        String imgURL = "https://picsum.photos/200";
+        Glide.with(this).load(imgURL).apply(RequestOptions.bitmapTransform(new RoundedCorners(18))).skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.NONE).into(imageView);
+    }
+}
